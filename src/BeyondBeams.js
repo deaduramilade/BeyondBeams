4e8b468566e56ab3dbe1ddf94989d6ddb6bed1f3
Samuel Okunribido <samuel.okunribido@icdfa.edu.ng>
2026-08-14 06:22:33 +0100

    Rebranded and production readiness

---
+++ src/BeyondBeams.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const RealTimeDefenseAgent = require('./a2spa-crypto/RealTimeDefenseAgent');
+const ComplianceAutomationAgent = require('./a2spa-crypto/ComplianceAutomationAgent');
+const PredictiveAnalyticsAgent = require('./a2spa-crypto/PredictiveAnalyticsAgent');
+const RegulatoryOversightAgent = require('./a2spa-crypto/RegulatoryOversightAgent');
+const RightsManagementAgent = require('./a2spa-crypto/RightsManagementAgent');
+
+const ROUTES = Object.freeze({
+  'realtime.defense.breach.detect': payload => RealTimeDefenseAgent.executeBreachDetection(payload),
+  'compliance.automation.dpia.generate': payload => ComplianceAutomationAgent.generateDPIA(payload),
+  'predictive.analytics.risk.model': payload => PredictiveAnalyticsAgent.runRiskModel(payload),
+  'regulatory.oversight.perform': payload => RegulatoryOversightAgent.performOversight(payload),
+  'rights.management.exercise': payload => RightsManagementAgent.exerciseRight(payload)
+});
+
+class BeyondBeams {
+  async execute(actionType, payload) {
+    const route = ROUTES[actionType];
+    if (!route) {
+      const error = new Error('Unknown action type');
+      error.code = 'UNKNOWN_ACTION';
+      throw error;
+    }
+    return route(payload);
+  }
+}
+
+module.exports = new BeyondBeams();
